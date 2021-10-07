7f874ea567ed040e9ae7d87aafe4af04b1647cf1
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-07 10:56:52 +0530

    Create poke.js

---
+++ plugins/poke.js
@@ -0,0 +1,20 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text }) => {
+  if (!text) throw `Enter a query!`
+  let res = await fetch(global.API('http://zekais-api.herokuapp.com', '/pokemon?query=', { q: text }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  let { title, gender, species, baseExp, height, weight, image } = json.results[0]
+let animeingfo = `✨️ *Name:* ${title}
+🎆️ *Species:* ${species}
+💌️ *Gender:* ${gender}
+❤️ *Height:* ${height}
+💚️ *Weight:* ${weight}
+🌐️ *BaseExp*: ${baseExp}`
+  conn.sendFile(m.chat, image, '', animeingfo, m)
+}
+handler.help = ['poke <title>']
+handler.tags = ['poke']
+handler.command = /^(poke|pokemon)$/i
+//maapin fatur :<
+module.exports = handler

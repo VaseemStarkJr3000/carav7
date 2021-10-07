32820cbea3695113aa781f21d875b3f77b1566af
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-07 06:31:53 +0530

    Create Kiss.js

---
+++ plugins/Kiss.js
@@ -0,0 +1,23 @@
+let fetch = require("node-fetch")
+const { sticker } = require('../lib/sticker')
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn}) => {
+  try {
+  let res = await fetch('https://api.waifu.pics/sfw/kiss')
+  let json = await res.json()
+  let { 
+url
+} = json
+let stiker = await sticker(null, url, 'kiss', 'cara')
+  conn.sendMessage(m.chat, stiker, MessageType.sticker, {
+    quoted: m
+  })
+ } catch (e) {
+  }
+}
+handler.help = ['kiss']
+handler.tags = ['expression']
+handler.command = /^kiss/i
+
+module.exports = handler

423999873312c6e36c936e810820d031fe39e02f
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-07 09:17:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -24,21 +24,21 @@ global.APIKeys = { // APIKey nya disini
   'https://neoxr-api.herokuapp.com': 'yntkts',
   'https://fxc7-api.herokuapp.com':'APIKEY',
   'https://pencarikode.xyz': 'pais',
-  'https://api.xteam.xyz': 'isi apikey lu',
-  'https://api.lolhuman.xyz': 'isi pake apikey lu',
+  'https://api.xteam.xyz': '6562d9833062f347',
+  'https://api.lolhuman.xyz': '8822f6044304d619f0fd0df7',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.xyz': 'apivinz',
+  'https://api.zeks.xyz': '5LHO6oeleNGl1PMJ0RkzwvU3W01',
 }
 
 // Sticker WM
-global.packname = 'Eva'
-global.author = 'Anirudh'
+global.packname = 'cara'
+global.author = 'let the world destroy'
 
 global.wait = '_*Please Wait..*_'
 global.eror = '_*Server Error*_'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
-global.watermark = '© EVA'
+global.watermark = '© Cara'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 

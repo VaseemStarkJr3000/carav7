f521a8d8fc4fdedf5b216e5be7f23a0240fe4453
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-08 11:31:09 +0530

    Update profile.js

--- plugins/profile.js
+++ plugins/profile.js
@@ -1,6 +1,6 @@
 let PhoneNumber = require('awesome-phonenumber')
 let levelling = require('../lib/levelling')
-let handler = async (m, { conn, usedPrefix }) => {
+let handler = async (m, { conn }) => {
   let pp = './src/avatar_contact.png'
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   try {
@@ -9,45 +9,23 @@ let handler = async (m, { conn, usedPrefix }) => {
 
   } finally {
     let about = (await conn.getStatus(who).catch(console.error) || {}).status || ''
-    if (typeof global.db.data.users[who] == "undefined") {
-      global.db.data.users[who] = {
-        exp: 0,
-        limit: 10,
-        lastclaim: 0,
-        registered: false,
-        name: conn.getName(m.sender),
-        age: -1,
-        regTime: -1,
-        afk: -1,
-        afkReason: '',
-        banned: false,
-        level: 0,
-        call: 0,
-        role: 'Warrior V',
-        autolevelup: false,
-        pc: 0,
-      }
-    }
-    let { name, limit, exp, lastclaim, registered, regTime, age, level, role, banned } = global.db.data.users[who]
+    let { name, limit, exp, lastclaim, registered, regTime, age, level } = global.DATABASE.data.users[who]
     let { min, xp, max } = levelling.xpRange(level, global.multiplier)
     let username = conn.getName(who)
-    let math = max - xp
     let str = `
-🎍 *Name:* ${username} ${registered ? '(' + name + ') ' : ''}(@${who.replace(/@.+/, '')})${about != 401 ? '\nInfo: ' + about : ''}
-
-🎐 *Number:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
-
-🀄 *Link:* https://wa.me/${who.split`@`[0]}${registered ? '\nUmur: ' + age : ''}
-
-🕹 *Premium:* ${prems.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(m.sender) ? 'Yes' : 'No'}
-
-🎗 *Registered:* ${registered ? 'Ya (' + new Date(regTime).toLocaleString() + ')' : 'Registered'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim).toLocaleString() : ''}
+Name: ${username} ${registered ? '(' + name + ') ': ''}(@${who.replace(/@.+/, '')})${about ? '\nAbout: ' + about : ''}
+Number: ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
+Link: https://wa.me/${who.split`@`[0]}${registered ? '\nAge: ' + age : ''}
+XP: TOTAL ${exp} (${exp - min} / ${xp}) [${max - exp} left to levelup]
+Level: ${level}
+Limit: ${limit}
+Registered: ${registered ? 'Yes (' + new Date(regTime) + ')': 'No'}${lastclaim > 0 ? '\nLast Claim: ' + new Date(lastclaim) : ''}
 `.trim()
     let mentionedJid = [who]
-    conn.sendFile(m.chat, pp, 'pp.jpg', banned ? 'if you get banned' : str, m, false, { contextInfo: { mentionedJid } })
+    conn.sendFile(m.chat, pp, 'pp.jpg', str, m, false, { contextInfo: { mentionedJid }})
   }
 }
 handler.help = ['profile [@user]']
 handler.tags = ['tools']
-handler.command = /^profile?$/i
+handler.command = /^profile$/i
 module.exports = handler

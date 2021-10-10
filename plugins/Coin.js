1bf97cdd5e5a3e86441a8e5252c6b833e56641a3
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-10 21:49:33 +0530

    Update Coin.js

--- plugins/Coin.js
+++ plugins/Coin.js
@@ -1,18 +1,35 @@
-let handler = async(m, { conn }) => {
+const { MessageType } = require("@adiwajshing/baileys");
+const { createSticker, StickerTypes } = require("wa-sticker-formatter");
+let axios = require("axios");
 
-    if(m.quoted?.sender) m.mentionedJid.push(m.quoted.sender)
-    if(!m.mentionedJid.length) m.mentionedJid.push(m.sender)
-  
- const data = ["https://tenor.com/bCh3q.gif","https://tenor.com/bCh3q.gif"]
-  const HUG = data[Math.floor(Math.random() * data.length)]
+let handler = async (m, { conn }) => {
+	let res = await axios.get(
+		API("https://no-api-key.com", "/api/v2/coin-flip")
+	);
+	if (res.status !== 200) throw await `${res.status} ${res.statusText}`;
+	const sticker = await createSticker(res.data.gif, {
+		type: StickerTypes.FULL,
+		pack: global.packname,
+		author: global.author,
+	});
+	await conn.sendMessage(m.chat, sticker, MessageType.sticker, {
+		quoted: m,
+		mimetype: "image/webp",
+	});
+	const head = await createSticker(res.data.image, {
+		type: StickerTypes.FULL,
+		pack: global.packname,
+		author: global.author,
+	});
+	await conn.sendMessage(m.chat, head, MessageType.sticker, {
+		quoted: m,
+		mimetype: "image/webp",
+	});
+	await m.reply(res.data.coin);
+};
+handler.help = ["coin"];
+handler.tags = ["fun"];
 
-  conn.sendFile(m.chat,HUG,'h.gif'`,m,false,
-  { mimetype :'video/gif',  contextInfo :{mentionedJid : [  ...m.mentionedJid,m.sender ] }})
-  
+handler.command = /^((coin)?flip)$/i;
 
-}
-handler.help = ['neko']
-handler.tags = ['internet']
-handler.command = /^coinflip$/i
-
-module.exports = handler
+module.exports = handler;

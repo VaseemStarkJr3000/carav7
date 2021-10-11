9c92d2607f89a6fc5d810a3c13aca1c3d0a4ac9f
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-11 10:47:39 +0530

    Update caraai.js

--- plugins/caraai.js
+++ plugins/caraai.js
@@ -1,29 +1,44 @@
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
+const axios = require('axios')
 
-    if (!text) return conn.reply(m.chat, 'Harap Masukan Nama Film Animenya', m)
 
-	axios.get(`https://api.brainshop.ai/get?bid=159811&key=S1IFzsWHWPjwi9ah&uid=[uid]&msg=${text}`).then ((res) => {
-	 	let hasil = `${res.data.cnt}`
 
-    conn.reply(m.chat, hasil, m)
-	})
+
+let handler = async (m, { conn, args  , usedPrefix, command })=>{
+
+
+if(!args[0]) throw " please provide place or location name"
+  
+    try{
+
+        const response = axios.get(`http://api.brainshop.ai/get?bid=159811&key=S1IFzsWHWPjwi9ah&uid=[uid]&msg=${args[0]}`)
+        const res = await response
+        
+        const name = res.data.cnt
+        const Country = res.data.sys.country
+        const Weather= res.data.weather[0].description
+        const Temperature = res.data.main.temp + '°C'
+        const Minimum_Temperature= res.data.main.temp_min + '°C'
+        const Maximum_Temperature= res.data.main.temp_max + '°C'
+        const Humidity= res.data.main.humidity + '%'
+        const Wind= res.data.wind.speed + 'km/h'
+        
+
+        conn.reply(m.chat,`
+        🌸 Place: ${name}\n💮 Country: ${cnt}\n🌈 Weather: ${Weather}\n🎋 Temperature: ${Temperature}\n💠 Minimum Temperature: ${Minimum_Temperature}\n📛 Maximum Temperature: ${Maximum_Temperature}\n💦 Humidity: ${Humidity}\n🎐 Wind: ${Wind}
+        `.trim(),m)
+    }catch(e){
+throw 'location not found' 
+console.log(e)
+
+    }
+
+
+
+
 }
-handler.help = ['bot'].map(v => v + ' <film>')
-handler.tags = ['nime']
-handler.command = /^(bot)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = true
+handler.help = ['cara']
+handler.tags = ['internet']
+handler.command = /^(cara|bot)$/i
 
 module.exports = handler

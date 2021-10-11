a3675db571df152b5c77075fae1d1e9f10b83e31
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-11 10:28:08 +0530

    Update caraai.js

--- plugins/caraai.js
+++ plugins/caraai.js
@@ -1,20 +1,17 @@
-let fetch = require('node-fetch')
-let googleIt = require('google-it')
-let handler = async (m, { conn, command, args }) => {
-  let full = /f$/i.test(command)
-  let text = args.join` `
-  if (!text) return conn.reply(m.chat, 'Tidak ada teks untuk di cari', m)
-  let url = 'https://api.brainshop.ai/get?bid=159811&key=S1IFzsWHWPjwi9ah&uid=[uid]&msg=' + encodeURIComponent(text)
-  let search = await googleIt({ query: text })
-  let msg = search.map(({ title, cnt, snippet}) => {
-    return `*${cnt}*`
-  }).join`\n\n`
-  let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).buffer()
-  conn.sendFile(m.chat, ss, 'screenshot.png', url + '\n\n' + msg, m)
+let axios = require("axios");
+let handler = async(m, { conn, text }) => {
+
+    if (!text) return conn.reply(m.chat, 'Harap Masukan Nama Film Animenya', m)
+
+	axios.get(`https://api.brainshop.ai/get?bid=159811&key=S1IFzsWHWPjwi9ah&uid=[uid]&msg=${text}`).then ((res) => {
+	 	let hasil = `${res.data.cnt}`
+
+    conn.reply(m.chat, hasil, m)
+	})
 }
-handler.help = ['cara', 'googlef'].map(v => v + ' <pencarian>')
-handler.tags = ['internet']
-handler.command = /^cara?$/i
+handler.help = ['bot'].map(v => v + ' <film>')
+handler.tags = ['nime']
+handler.command = /^(bot)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
@@ -26,5 +23,7 @@ handler.admin = false
 handler.botAdmin = false
 
 handler.fail = null
+handler.exp = 0
+handler.limit = true
 
 module.exports = handler

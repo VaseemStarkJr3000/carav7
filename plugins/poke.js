5d2798949db4757c6778874d600cb01c5f73cc9f
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-14 12:49:37 +0530

    Update poke.js

--- plugins/poke.js
+++ plugins/poke.js
@@ -1,109 +1,51 @@
-let imageToBase64 = require('image-to-base64');
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
+let fetch = require("node-fetch");
 
-    if (!text) return conn.reply(m.chat, 'Masukan Username yang akan distalk', m)
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `Pokemonnya mana?`;
+  let res = await fetch(
+    API("https://some-random-api.ml", "/pokedex", { pokemon: args[0] })
+  );
+  if (!res.ok) throw `${res.status} ${res.statusText}`;
+  let json = await res.json();
+  let pokedex = `
+Name: ${json.name}
+Id: ${json.id}
+Type: ${json.type}
+Species: ${json.species}
+Abilites: ${json.abilities}
+Height: ${json.height}
+Weight: ${json.weight}
+Base experience: ${json.base_experience}
+Gender: ${json.gender}
+Egg groups: ${json.egg_groups}\n
+STATS
+Hp: ${json.stats.hp}
+Attack: ${json.stats.attack}
+Defense: ${json.stats.defense}
+Sp atk: ${json.stats.sp_atk}
+Sp def: ${json.stats.sp_def}
+Speed: ${json.stats.speed}
+Total: ${json.stats.total}\n
+FAMILY
+Evolution Stage: ${json.family.evolutionStage}
+Evolution Line: ${json.family.evolutionLine}\n
+DESCRIPTION
+${json.description}
+Generation: ${json.generation}\n\n
+~fatur
+  `.trim();
+  if (!json.error)
+    await conn.sendFile(
+      m.chat,
+      json.sprites.animated,
+      "pokemon.gif",
+      pokedex,
+      m
+    );
+  else throw json.error;
+};
+handler.help = ["pokemon"].map((v) => v + " <pokemon>");
+handler.tags = ["internet"];
+handler.command = /^(pokemon|pokedex)$/i;
 
-  await m.reply('Searching...')
-    axios.get('let imageToBase64 = require('image-to-base64');
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
-
-    if (!text) return conn.reply(m.chat, 'Masukan Username yang akan distalk', m)
-
-  await m.reply('Searching...')
-    axios.get('let imageToBase64 = require('image-to-base64');
-let axios = require("axios");
-let handler = async(m, { conn, text }) => {
-
-    if (!text) return conn.reply(m.chat, 'Masukan Username yang akan distalk', m)
-
-  await m.reply('Searching...')
-    axios.get('http://zekais-api.herokuapp.com/pokemon?query=' + text)
-    .then((res) => {
-      imageToBase64(res.data.image)
-        .then(
-          (ress) => {
-            let buf = Buffer.from(ress, 'base64')
-            let hasil = `Name : ${res.data.title}\nType : ${res.data.species}\nHeight : ${res.data.height}\nWeight : ${res.data.weight}\nGender : ${res.data.gender}\nBaseExp : ${res.data.baseExp}`
-
-    conn.reply(m.chat, buf, 'foto.jpg', hasil, m)
-         })
-	})
-}
-handler.help = ['poke','pokemon'].map(v => v + ' <@user>')
-handler.tags = ['internet']
-handler.command = /^(poke|pokemon)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = true
-
-module.exports = ' + text)
-    .then((res) => {
-      imageToBase64(res.data.Profile_pic)
-        .then(
-          (ress) => {
-            let buf = Buffer.from(ress, 'base64')
-            let hasil = `*IG STALKER*\n\nUsername : ${res.data.Username}\nFullName : ${res.data.name}\nFollowers : ${res.data.Jumlah_Followers}\nFollowing : ${res.data.Jumlah_Following}\nPost : ${res.data.Jumlah_Post}\nBio : ${res.data.Biodata}`
-
-    conn.reply(m.chat, buf, 'foto.jpg', hasil, m)
-         })
-	})
-}
-handler.help = ['igstalk','stalkig'].map(v => v + ' <@user>')
-handler.tags = ['internet']
-handler.command = /^(igstalk|stalkig)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = true
-
-module.exports = ' + text)
-    .then((res) => {
-      imageToBase64(res.data.Profile_pic)
-        .then(
-          (ress) => {
-            let buf = Buffer.from(ress, 'base64')
-            let hasil = `*IG STALKER*\n\nUsername : ${res.data.Username}\nFullName : ${res.data.name}\nFollowers : ${res.data.Jumlah_Followers}\nFollowing : ${res.data.Jumlah_Following}\nPost : ${res.data.Jumlah_Post}\nBio : ${res.data.Biodata}`
-
-    conn.reply(m.chat, buf, 'foto.jpg', hasil, m)
-         })
-	})
-}
-handler.help = ['igstalk','stalkig'].map(v => v + ' <@user>')
-handler.tags = ['internet']
-handler.command = /^(igstalk|stalkig)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = true
-
-module.exports = handler
+module.exports = handler;

9c446a9c58a44b80b95830d1389539b84e552b58
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-15 10:11:34 +0530

    Update Gchara.js

--- plugins/Gchara.js
+++ plugins/Gchara.js
@@ -1,35 +1,31 @@
 let fetch = require('node-fetch')
+let timeout = 60000
+let poin = 25000
 
-let timeout = 120000
-let poin = 500
-let handler = async (m, { conn, usedPrefix }) => {
-    conn.tebakkimia = conn.tebakkimia ? conn.tebakkimia : {}
+let handler  = async (m, { conn, usedPrefix }) => {
+
+    conn.tebakanime = conn.tebakanime ? conn.tebakanime : {}
     let id = m.chat
-    if (id in conn.tebakkimia) {
-        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkimia[id][0])
+    if (id in conn.tebakanime) {
+        conn.reply(m.chat, 'There are still unanswered questions in this chat', conn.tebakanime[id][0])
         throw false
     }
-    let res = await fetch(global.API('http://zekais-api.herokuapp.com', '/tebakunsur'))
+    let res = await fetch('http://zekais-api.herokuapp.com/tebakanime')
     if (res.status !== 200) throw await res.text()
     let json = await res.json()
-    if (json.status != 200) throw json
-    let caption = `
-Nama unsur dari lambang ${json.simbol} adalah...
-Timeout *${(timeout / 1000).toFixed(2)} detik*
-Ketik ${usedPrefix}teki untuk bantuan
-Bonus: ${poin} XP
-`.trim()
-    conn.tebakkimia[id] = [
-        await conn.reply(m.chat, caption, m),
-        json, poin,
-        setTimeout(() => {
-            if (conn.tebakkimia[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.name}*`, conn.tebakkimia[id][0])
-            delete conn.tebakkimia[id]
-        }, timeout)
+    if (!json.status) throw json
+    let caption = `GUESS CHARACTER \n\nTime : ${(timeout / 1000).toFixed(2)} Seconds\nBonus : ${poin} XP`
+    conn.tebakanime[id] = [
+      await conn.sendFile(m.chat, json.image, 'tebakanime.jpg', caption, m),
+      json, poin,
+      setTimeout(() => {
+        if (conn.tebakanime[id]) conn.reply(m.chat, `Time Out!\nThe answer is ${json.name}\n\nUrl : ${json.url}\nDesk :\n${json.desc}`, conn.tebakanime[id][0])
+        delete conn.tebakanime[id]
+      }, timeout)
     ]
-}
+  }
 handler.help = ['haigusha']
 handler.tags = ['game']
-handler.command = /^haigusha/i
+handler.command = /^haigusha$/i
 
 module.exports = handler

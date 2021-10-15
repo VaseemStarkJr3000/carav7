8398868391d840b24d4f65a9f6ba3312a8457275
Dasabae <85019441+iamherok@users.noreply.github.com>
2021-10-15 10:12:31 +0530

    Update Gcharaans.js

--- plugins/Gcharaans.js
+++ plugins/Gcharaans.js
@@ -1,22 +1,20 @@
 let handler = m => m
+
 handler.before = async function (m) {
-    let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*teki/i.test(m.quoted.text)) return !0
-    conn.tebakkimia = conn.tebakkimia ? conn.tebakkimia : {}
-    if (!(id in conn.tebakkimia)) return m.reply('Soal itu telah berakhir')
-    if (m.quoted.id == conn.tebakkimia[id][0].id) {
-        let json = JSON.parse(JSON.stringify(conn.tebakkimia[id][1]))
-        // m.reply(JSON.stringify(json, null, '\t'))
-        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.DATABASE._data.users[m.sender].exp += conn.tebakkimia[id][2]
-            m.reply(`*Benar!*\n+${conn.tebakkimia[id][2]} XP`)
-            clearTimeout(conn.tebakkimia[id][3])
-            delete conn.tebakkimia[id]
-        } else if (m.text.toLowerCase().endsWith(json.name.split` `[1])) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
-    }
-    return !0
+
+  let id = m.chat
+  if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/GUESS CHARACTER/i.test(m.quoted.text)) return
+  conn.tebakanime = conn.tebakanime ? conn.tebakanime : {}
+  if (!(id in conn.tebakanime)) return m.reply('The matter has ended')
+  if (m.quoted.id == conn.tebakanime[id][0].id) {
+    let json = JSON.parse(JSON.stringify(conn.tebakanime[id][1]))
+    if (m.text.toLowerCase() == json.name.toLowerCase()) {
+      global.db._data.users[m.sender].exp += conn.tebakanime[id][2]
+      m.reply(`Correct answer!\n+${conn.tebakanime[id][2]} XP`)
+      clearTimeout(conn.tebakanime[id][3])
+      delete conn.tebakanime[id]
+    } else if (m.text.toLowerCase().endsWith(json.name.split` `[1])) m.reply(`A Little More!`)
+    else m.reply(`Correct answer!`)
+  }
 }
 handler.exp = 0
-
-module.exports = handler
